bc96b30f75532f73ff7bd0dd1e8e1b45fc53361c
nemonemonee <88709397+nemonemonee@users.noreply.github.com>
2023-03-14 22:46:34 -0500

    Add files via upload

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -11,6 +11,7 @@ class PARALLEL_HILL_CLIMBER:
         os.system("rm fitness*.txt")
         self.nextAvailableID = 0
         self.parents = dict()
+        numpy.random.seed(31)
         for i in range(c.populationSize):
             self.parents[i] = SOLUTION(self.nextAvailableID)
             self.nextAvailableID += 1
@@ -23,7 +24,7 @@ class PARALLEL_HILL_CLIMBER:
             self.Evolve_For_One_Generation()
             if (self.currentGeneration + 1) % 100 == 0:
                 for p in self.parents:
-                    self.parents[p].save()
+                    self.parents[p].save(p)
     
     def Evolve_For_One_Generation(self):
         self.Spawn()
@@ -40,15 +41,15 @@ class PARALLEL_HILL_CLIMBER:
 
     def Mutate(self):
         for i in self.children:
-            self.children[i].MutateBody()
-            self.children[i].SwapSensors()
-            self.children[i].MutateBrain()
-#            if self.currentGeneration % 3 == 1:
-#                self.children[i].MutateBody()
-#            elif self.currentGeneration % 3 == 2:
+#            self.children[i].MutateBody()
+#            self.children[i].SwapSensors()
+#            self.children[i].MutateBrain()
+            if self.currentGeneration % 2 == 0:
+                self.children[i].MutateBody()
+            elif self.currentGeneration % 2 == 1:
 #                self.children[i].SwapSensors()
 #            else:
-#                self.children[i].MutateBrain()
+                self.children[i].MutateBrain()
         
     def Select(self):
         for i in self.parents:
@@ -68,11 +69,11 @@ class PARALLEL_HILL_CLIMBER:
         
     def Show_Best(self):
         t = 0
-        input()
         print(self.fitnessRecords)
         numpy.save("fitData.npy", self.fitnessRecords)
         top5 = numpy.zeros(5)
         for i in self.parents:
+            self.parents[i].save(i)
             self.parents[i].Start_Simulation("GUI")
 
 
--- solution.py
+++ solution.py
@@ -9,7 +9,7 @@ import pickle
 class SOLUTION:
     def __init__(self, id):
         self.myID = id
-        numpy.random.seed(id)
+        #numpy.random.seed(id)
         self.InitializeRandomVariables()
         self.BuildBodyPlan()
         self.ArrangementPlan()
@@ -41,9 +41,9 @@ class SOLUTION:
         
     def Create_World(self):
         pyrosim.Start_SDF("world.sdf")
-        pyrosim.Send_Cube(name="Box0", pos=[-5,0,.2] , size=[5,10,.5])
-        for i in range(1,10):
-            pyrosim.Send_Cube(name="Box"+str(i), pos=[-5-5*i,0,.2+.5*i] , size=[5,10,.5+i])
+        pyrosim.Send_Cube(name="Box0", pos=[-5,0,.2] , size=[5,20,1])
+#        for i in range(1,10):
+#            pyrosim.Send_Cube(name="Box"+str(i), pos=[-5-5*i,0,.2+.5*i] , size=[5,10,.5+i])
         pyrosim.End()
 
     def Create_Body(self):
@@ -55,17 +55,12 @@ class SOLUTION:
         for j in self.bodyPlan[0]:
             center2pivot = allDir[self.growDir[j]]
             self.abs_pos[j] = self.abs_pos[0]+center2pivot*.5*(self.size[0]+self.size[j])
-            
             ball = "m0" + str(j)
             pyrosim.Send_Joint(name="0_"+ball, parent="0", child=ball, type="revolute", position=self.abs_pos[0]+self.size[0]*.5*center2pivot, jointAxis = "1 0 0")
-            
             pyrosim.Send_Cube(name=ball, pos=[0,0,0], size=[0,0,0], colorName="Blue", colorString='    <color rgba="0 0 1.0 1.0"/>')
-            
             pyrosim.Send_Joint(name=ball+"_"+str(j), parent=ball, child=str(j), type="revolute", position=[0,0,0], jointAxis = "0 1 1")
-            
             pyrosim.Send_Cube(name=str(j), pos=self.size[j]*.5*center2pivot, size=self.size[j], colorName=self.GetColor(j)[0], colorString=self.GetColor(j)[1])
-        
-        
+
         for i in self.bodyPlan:
             if i != 0:
                 pivot2center = allDir[self.growDir[i]]
@@ -73,15 +68,10 @@ class SOLUTION:
                     center2pivot = allDir[self.growDir[j]]
                     pivot2pivot = (center2pivot + pivot2center) * 0.5
                     self.abs_pos[j] = self.abs_pos[i]+center2pivot*.5*(self.size[i]+self.size[j])
-                    
                     ball = "m" + str(i) + str(j)
-                    
                     pyrosim.Send_Joint(name=str(i)+"_"+ball, parent=str(i), child=ball, type="revolute", position=self.size[i]*pivot2pivot, jointAxis="1 1 0")
-                    
                     pyrosim.Send_Cube(name=ball, pos=[0,0,0], size=[0,0,0], colorName="Blue", colorString='    <color rgba="0 0 1.0 1.0"/>')
-                    
                     pyrosim.Send_Joint(name=ball+"_"+str(j), parent=ball, child=str(j), type="revolute", position=[0,0,0], jointAxis="0 1 1")
-                    
                     pyrosim.Send_Cube(name=str(j), pos=self.size[j]*0.5*center2pivot, size=self.size[j], colorName=self.GetColor(j)[0], colorString=self.GetColor(j)[1])
         pyrosim.End()
         
@@ -90,7 +80,7 @@ class SOLUTION:
         pyrosim.Start_NeuralNetwork("brain" + str(self.myID) + ".nndf")
         k = 0
         for i in self.bodyPlan:
-            if (self.sensorOrNot[i]):
+            if (self.sensorOrNot[numpy.abs(i)]):
                 pyrosim.Send_Sensor_Neuron(name = k, linkName = str(i))
                 k += 1
         for i in self.bodyPlan:
@@ -100,8 +90,8 @@ class SOLUTION:
                 k += 1
                 pyrosim.Send_Motor_Neuron(name = k, jointName = ball+"_"+str(j))
                 k += 1
-        for i in range(self.numSensorNeurons):
-            for j in range(self.numMotorNeurons):
+        for i in range(self.weights.shape[0]):
+            for j in range(self.weights.shape[1]):
                 pyrosim.Send_Synapse(sourceNeuronName = i, targetNeuronName = j + self.numSensorNeurons, weight = self.weights[i][j])
         pyrosim.End()
     
@@ -113,42 +103,62 @@ class SOLUTION:
     # Then it will calculate the the number of sensors and motors, and intialize a
     #  weight matrix based on those numbers.
     def InitializeRandomVariables(self):
-        self.length = 2 + numpy.random.randint(7)
+        self.length = 2 + numpy.random.randint(2)
         self.sensorOrNot = numpy.random.rand(self.length) < 0.5
         if numpy.sum(self.sensorOrNot) == 0: self.sensorOrNot[0] = True
         self.size = .1 + numpy.random.rand(self.length, 3) * 1.9
-        self.numSensorNeurons = numpy.sum(self.sensorOrNot)
-        self.numMotorNeurons = 2 * (self.length - 1)
+        self.numSensorNeurons = 2 * numpy.sum(self.sensorOrNot)
+        self.numMotorNeurons = 4 * (self.length - 1)
         self.weights = numpy.random.rand(self.numSensorNeurons,self.numMotorNeurons) * 2 - 1
         self.toBotm = self.size[0,2] / 2
     
     def BuildBodyPlan(self):
-        self.bodyPlan = {0:[1], 1:[]}
+        self.bodyPlan = {0:[1, -1], 1:[], -1:[]}
         potentialParent = [0, 1]
         for i in range(2, self.length):
           pre_i = numpy.random.choice(potentialParent)
           self.bodyPlan[pre_i].append(i)
-          if len(self.bodyPlan[pre_i]) >= 3: potentialParent.remove(pre_i)
+          self.bodyPlan[-pre_i].append(-i)
+          if len(self.bodyPlan[pre_i]) > 2: potentialParent.remove(pre_i)
           potentialParent.append(i)
           self.bodyPlan[i] = []
+          self.bodyPlan[-i] = []
+        #print(self.bodyPlan)
     
     def ArrangementPlan(self):
         self.growDir = {}
-        remainDir = {_:list(range(6)) for _ in range(self.length)}
-        remainDir[0].remove(3)
-        for i in range(len(self.bodyPlan)):
-           for j in self.bodyPlan[i]:
-             d = numpy.random.choice(remainDir[i])
-             remainDir[i].remove(d)
+        remainDir = {_:list(range(6)) for _ in range(-self.length, self.length)}
+        for j in self.bodyPlan[0]:
+           if j > 0:
+             d = numpy.random.choice(remainDir[0])
+             print(d)
+             remainDir[0].remove(d)
+             remainDir[0].remove(5-d)
              remainDir[j].remove(5-d)
+             remainDir[-j].remove(d)
              self.growDir[j] = d
-             if d == 3: self.toBotm += self.size[j,2]
-    
+             self.growDir[-j] = 5-d
+             if d == 3 or d == 2: self.toBotm += self.size[j,2]
+             
+        for i in self.bodyPlan:
+           if i > 0:
+               for j in self.bodyPlan[i]:
+                 if j > 0:
+                     d = numpy.random.choice(remainDir[i])
+                     remainDir[i].remove(d)
+                     remainDir[-i].remove(5-d)
+                     remainDir[j].remove(5-d)
+                     remainDir[-j].remove(d)
+                     self.growDir[j] = d
+                     self.growDir[-j] = 5-d
+                     if d == 3 or d == 2: self.toBotm += self.size[j,2]
+        #print(self.growDir)
     
     # It takes in the index of the cube
     # It decides the color based on if the cube has a sensor or not
     # It returns the color string and color name of the given cube
     def GetColor(self, idx):
+        idx = numpy.abs(idx)
         if self.sensorOrNot[idx]:
             return "Green", '    <color rgba="0 1.0 0 1.0"/>'
         else :
@@ -174,12 +184,19 @@ class SOLUTION:
         
     def Set_ID(self, id):
         self.myID = id
-        numpy.random.seed(id)
+        #numpy.random.seed(id)
         
-    def save(self):
-        filename = "save" + str(self.myID) + ".soln"
+    def save(self, idx):
+        filename = "sym" + str(idx) + ".soln"
+        config_dict  = {"body":self.bodyPlan, "brain":self.weights, "size":self.size, "sensor":self.sensorOrNot, "abs_pos":self.abs_pos, "grow_dir":self.growDir}
         with open(filename , 'wb') as outp:
-            pickle.dump(self, outp, pickle.HIGHEST_PROTOCOL)
+            pickle.dump(config_dict, outp, pickle.HIGHEST_PROTOCOL)
     
     def load(self, filename):
-        self = pickle.load( open(filename, "rb" ) )
+        config_dict = pickle.load( open(filename, "rb" ) )
+        self.bodyPlan = config_dict["body"]
+        self.weights = config_dict["brain"]
+        self.size = config_dict["size"]
+        self.sensorOrNot = config_dict["sensor"]
+        self.abs_pos = config_dict["abs_pos"]
+        self.growDir = config_dict["grow_dir"]
